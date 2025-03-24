daacc3d338a50b6851d94e6c3cee49298f562681
Nikola Losic <nikolalosic@protonmail.ch>
2025-03-24 09:51:26 +0100

    Add example

--- test.py
+++ test.py
@@ -1,87 +1,57 @@
-import pandas as pd
-from ziplime.protocol import BarData
-from zipline.api import symbol, order_target, record
-from zipline import TradingAlgorithm
+import datetime
+
+from ziplime.algorithm import TradingAlgorithm
+from ziplime.domain.bar_data import BarData
 
 from ziplime.utils.bundle_utils import register_default_bundles
-from ziplime.utils.data import get_fundamental_data
-
-from ziplime.data.bundles import load
-from ziplime.utils.paths import data_path
 
 
-def initialize(context: TradingAlgorithm):
-    context.asset = symbol('NFLX')
-    context.i = 0
+
+register_default_bundles()
+
+
+def initialize(context):
+    context.assets = [context.symbol('AAPL')]
+    context.assett = context.symbol("AMZN")  # Apple and Amazon
+    context.counter = 0
 
 
 def handle_data(context: TradingAlgorithm, data: BarData):
-    # Skip first 300 days to get full windows
-    # order_target(context.asset, 100)
+    context.counter += 1
+    print(f"Handle data for: {context.datetime}")
 
-    context.i += 1
-    if context.i < 5:
-        return
-    data.history(context.asset, 'price', bar_count=3, frequency="1d")
-    data.history([symbol('NFLX'), symbol('AAPL')], 'price', bar_count=3, frequency="1d")
-
-    # Compute averages
-    # data.history() has to be called with the same params
-    # from above and returns a pandas dataframe.
-    short_mavg = data.history(context.asset, 'price', bar_count=1, frequency="1d").mean()
-    long_mavg = data.history(context.asset, 'price', bar_count=1, frequency="1d").mean()
-    return_on_tangible_equity_mean = get_fundamental_data(
-        bar_data=data, context=context, assets=context.asset,
-        fields='return_on_tangible_equity_value', bar_count=32,
-        frequency="1q", fillna=None
-    )
-
-    print(return_on_tangible_equity_mean)
-    # Trading logic
-    if short_mavg > long_mavg:
-        # order_target orders as many shares as needed to
-        # achieve the desired number of shares.
-        order_target(context.asset, 100)
-    elif short_mavg < long_mavg:
-        order_target(context.asset, 0)
-
-    # Save values for later inspection
-    record(
-        AAPL=data.current(context.asset, 'price'),
-        short_mavg=short_mavg,
-        long_mavg=long_mavg
-    )
-
-
-def get_benchmark_returns(start, end):
-    register_default_bundles()
-    bundle_data = load('lime')
-
-    spx_asset = bundle_data.asset_repository.lookup_symbol('AAPL', as_of_date=None)
-    spx_data = bundle_data.historical_data_reader.load_raw_arrays(
-        columns=['open', 'close'],
-        start_date=start,
-        end_date=end,
-        assets=[spx_asset],
-    )
-    spx_prices = pd.Series(spx_data[1].flatten(), index=pd.to_datetime(spx_data[0].flatten(), unit='s'))
-    spx_returns = spx_prices.pct_change().dropna()
-    return spx_returns
-
+    for asset in context.assets:
+        if context.counter > 10:
+            long_mavg_df = data.history(
+                assets=[asset, context.assett],
+                fields=['close'],
+                bar_count=10,
+                frequency=datetime.timedelta(minutes=3)
+            )
+            asset_series = long_mavg_df.filter(long_mavg_df["sid"] == asset.sid)["close"]
+            context.order(asset=asset, amount=10)
+            # print(asset_series)
 #
+# start_session = pd.Timestamp('2025-02-14')
 #
-# start_session = pd.Timestamp('2024-06-04')
-# end_session = pd.Timestamp('2024-10-01')
+# end_session = pd.Timestamp('2025-02-14')
 #
-# benchmark_returns = get_benchmark_returns(start_session, end_session)
+# param_capital = 100000
 #
-# result = run_algorithm(
-#     start=start_session,
-#     end=end_session,
-#     initialize=initialize,
-#     handle_data=handle_data,
-#     capital_base=100000,
-#     data_frequency='daily',
-#     bundle='lime',
-#     benchmark_returns=None
-# )
+# param_frequency = 'minute'
+#
+# param_bundle = 'limex_us_equity_min'
+#
+# result = run_algorithm(start=start_session,
+#                        end=end_session,
+#                        initialize=initialize,
+#                        handle_data=handle_data,
+#                        capital_base=param_capital,
+#                        data_frequency=param_frequency,
+#                        bundle=param_bundle)
+#
+# # report stats
+#
+# result_returns = result.returns
+#
+# print(result)
